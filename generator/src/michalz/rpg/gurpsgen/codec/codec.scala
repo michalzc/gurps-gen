76c46c317bf3b6ea75158fa98a569c0a253c1944
Michał Zając <michal.t.zajac@gmail.com>
2025-07-18 02:04:30 +0200

    Parser and its spec

--- generator/src/michalz/rpg/gurpsgen/codec/codec.scala
+++ generator/src/michalz/rpg/gurpsgen/codec/codec.scala
@@ -1,10 +1,14 @@
 package michalz.rpg.gurpsgen.codec
 
+import cats.syntax.traverse._
+
 import io.circe.Decoder
 import io.circe.Encoder
 import io.circe.HCursor
-import io.circe.generic.semiauto.*
-import michalz.rpg.gurpsgen.model.{Element, Generator, GeneratorElement, ListElements, NPCGenerator}
+import io.circe.JsonObject
+import io.circe.generic.semiauto._
+
+import michalz.rpg.gurpsgen.model._
 
 val ElementRegEx = """([^\[^\]]+)\s+\[([-\d][\d]*)\]""".r
 
@@ -15,9 +19,7 @@ given Decoder[Element] = Decoder.decodeString.emap {
 }
 
 given Encoder[Element] =
-  Encoder.encodeString.contramap(elem =>
-    s"${elem.name} [${elem.cost}]"
-  )
+  Encoder.encodeString.contramap(elem => s"${elem.name} [${elem.cost}]")
 
 given Decoder[ListElements] = Decoder
   .decodeList[GeneratorElement]
@@ -26,8 +28,18 @@ given Decoder[ListElements] = Decoder
 given Decoder[Generator] = deriveDecoder[Generator]
 
 given Decoder[GeneratorElement] =
-  (c: HCursor) => c.as[Element]
-    .orElse(c.as[ListElements])
-    .orElse(c.as[Generator])
+  (c: HCursor) =>
+    c.as[Element]
+      .orElse(c.as[ListElements])
+      .orElse(c.as[Generator])
 
-given Decoder[NPCGenerator] = deriveDecoder[NPCGenerator]
+given Decoder[NPCGenerator] = (c: HCursor) => {
+  for {
+    obj <- c.as[JsonObject]
+    lst <- obj.toList
+      .traverse((k, v) =>
+        v.as[GeneratorElement]
+          .map(vv => k -> vv)
+      )
+  } yield NPCGenerator(lst.toMap)
+}
--- generator/test/src/michalz/rpg/gurpsgen/model/CodecSpec.scala
+++ generator/test/src/michalz/rpg/gurpsgen/model/CodecSpec.scala
@@ -1,38 +1,31 @@
 package michalz.rpg.gurpsgen.model
 
+import scala.io.Source
+import scala.util.Using
+
 import io.circe.Json
 import io.circe.syntax._
+import io.circe.yaml.v12.parser.parse
+
 import org.scalatest.EitherValues
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 import michalz.rpg.gurpsgen.codec.given
+import michalz.rpg.gurpsgen.utils.orThrow
 
-class CodecSpec
-    extends AnyFreeSpec
-    with Matchers
-    with EitherValues {
+class CodecSpec extends AnyFreeSpec with Matchers with EitherValues {
   "A codec for" - {
     "Element" - {
       "should deserialize" - {
         "proper line" in {
           val json = "ST 11 [10]".asJson
-          json
-            .as[GeneratorElement]
-            .value shouldEqual Element(
-            "ST 11",
-            10
-          )
+          json.as[GeneratorElement].value shouldEqual Element("ST 11", 10)
         }
 
         "line with negative cost" in {
           val json = "Code of Honor 10 [-10]".asJson
-          json
-            .as[GeneratorElement]
-            .value shouldEqual Element(
-            "Code of Honor 10",
-            -10
-          )
+          json.as[GeneratorElement].value shouldEqual Element("Code of Honor 10", -10)
         }
       }
     }
@@ -40,42 +33,35 @@ class CodecSpec
     "ListElements" - {
       "should deserialize" - {
         "simple list of elements" in {
-          val json = Json.arr(
-            "Magic Resisance 1 [2]".asJson,
-            "Status 1 [5]".asJson,
-            "Code of Honor 10 [-10]".asJson
-          )
-          json
-            .as[GeneratorElement]
-            .value shouldEqual ListElements(
-            List(
-              Element("Magic Resisance 1", 2),
-              Element("Status 1", 5),
-              Element("Code of Honor 10", -10)
-            )
+          val json = Json.arr("Magic Resistance 1 [2]".asJson, "Status 1 [5]".asJson, "Code of Honor 10 [-10]".asJson)
+          json.as[GeneratorElement].value shouldEqual ListElements(
+            List(Element("Magic Resistance 1", 2), Element("Status 1", 5), Element("Code of Honor 10", -10))
           )
         }
 
-        "embedded list" in {
+        "nested list" in {
           val json = Json.arr(
             "Magic resistence [5]".asJson,
-            Json.arr(
-              "Elem 1 [5]".asJson,
-              "+2 ST [10]".asJson
-            ),
+            Json.arr("Elem 1 [5]".asJson, "+2 ST [10]".asJson),
             "Other [2]".asJson
           )
-
-          json
-            .as[GeneratorElement]
-            .value shouldEqual ListElements(
+          json.as[GeneratorElement].value shouldEqual ListElements(
             Element("Magic resistence", 5),
-            ListElements(
-              Element("Elem 1", 5),
-              Element("+2 ST", 10)
-            ),
+            ListElements(Element("Elem 1", 5), Element("+2 ST", 10)),
             Element("Other", 2)
           )
+        }
+
+        "list from yaml" in withYamlFromFile("/attributes.yml") { json =>
+          json.as[GeneratorElement].orThrow
+        }
+
+        "simple nested list from yaml" in withYamlFromFile("/simple-nested-list.yml") { json =>
+          json.as[GeneratorElement].orThrow
+        }
+
+        "nested lists from yaml" in withYamlFromFile("/nested-lists.yml") { json =>
+          json.as[GeneratorElement].orThrow
         }
       }
     }
@@ -83,19 +69,33 @@ class CodecSpec
     "Generator" - {
       "should deserialize" - {
         "one element generator" in {
-          val json = Json.obj(
-            "points"  -> 20.asJson,
-            "choices" -> "Some advantage [20]".asJson
-          )
-          json
-            .as[GeneratorElement]
-            .value shouldEqual Generator(
+          val json = Json.obj("points" -> 20.asJson, "choices" -> "Some advantage [20]".asJson)
+          json.as[GeneratorElement].value shouldEqual Generator(
             points = 20,
-            choices =
-              Element(name = "Some advantage", cost = 20)
+            choices = Element(name = "Some advantage", cost = 20)
           )
         }
       }
     }
+
+    "NPC Generator" - {
+      "should deserialize" - {
+        "simple generator from yaml file" in withYamlFromFile("/attributes-npc.yml") { json =>
+          json.as[NPCGenerator].orThrow
+        }
+        "generator from yaml file" in withYamlFromFile("/soldier-of-fortune.yml") { json =>
+          json.as[NPCGenerator].orThrow
+        }
+      }
+    }
+  }
+
+  def withFile[A](fileName: String)(code: String => A): A =
+    Using(Source.fromInputStream(getClass.getResourceAsStream(fileName))) { src =>
+      code(src.mkString)
+    }.orThrow
+
+  def withYamlFromFile[A](fileName: String)(code: Json => A): A = withFile(fileName) { yamlString =>
+    code(parse(yamlString).orThrow)
   }
 }
